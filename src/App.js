ad7ffb10411b7c43ab9a4792f4501b28e8625c72
anurag-10-git <anuragmehra2011@gmail.com>
2022-11-10 00:48:51 +0530

    memo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React, { useState} from "react";
-import Button from "./components/UI/Button/Button";
-import "./App.css";
-import DemoOutput from "./components/UI/Button/Demo/DemoOutput";
+import React, { useState } from 'react';
+
+import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
+import './App.css';
 
 function App() {
-const [showParagraph, setShowParagraph] = useState(false);
-console.log('this is App.js');
-const toggleParagraphHandler = () => {
-  setShowParagraph(prevShowParagraph => !prevShowParagraph);
-}
+  const [showParagraph, setShowParagraph] = useState(false);
 
+  console.log('APP RUNNING');
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+ 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false}/>
-      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
+      <DemoOutput show={false} />{/*React.memo compares new value with old state .false is boolean that is primitive value*/}
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>{/*comparision of two non-primitive values are always false even if they are same every element in js is unique*/}
     </div>
   );
 }
