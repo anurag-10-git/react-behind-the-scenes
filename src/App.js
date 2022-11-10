452afca0b4e1596a732c4c77f29b0222f4733278
anurag-10-git <anuragmehra2011@gmail.com>
2022-11-10 20:24:21 +0530

    how state works

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import Button from './components/UI/Button/Button';
 import DemoOutput from './components/Demo/DemoOutput';
@@ -6,17 +6,24 @@ import './App.css';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('APP RUNNING');
 
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
+    if(allowToggle)
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
+  },[allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  }
  
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />{/*React.memo compares new value with old state .false is boolean that is primitive value*/}
+      <DemoOutput show={showParagraph} />{/*React.memo compares new value with old state .false is boolean that is primitive value*/}
+      <Button onClick={allowToggleHandler}>Allow Toggle!</Button>{/*comparision of two non-primitive values are always false even if they are same every element in js is unique*/}
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>{/*comparision of two non-primitive values are always false even if they are same every element in js is unique*/}
     </div>
   );
